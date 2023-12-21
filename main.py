1bb648105235f98acad98b15559c1ac1d706fcde
MightiD <128378210+MightiD@users.noreply.github.com>
2023-12-21 15:47:56 +0000

    Ping Command added (First command)

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ logger = settings.logging.getLogger("bot")
 
 def run():
     intents = discord.Intents.default()
+    intents.message_content = True
 
     bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    @bot.command(
+        aliases=["p", "P"],
+        help="Replies with Pong",
+        brief="Replies with Pong"
+    )
+    async def ping(ctx):
+        """Answers with Pong"""
+        await ctx.reply("pong")
 
     bot.run(settings.API_TOKEN, root_logger=True)
 
